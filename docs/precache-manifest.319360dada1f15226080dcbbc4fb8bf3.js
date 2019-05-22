516b6884e870f0d8aa60da2fbc93c24c0e7dc525
Tomas Hubelbauer <tomas@hubelbauer.net>
2019-05-22 13:21:28 +0200

    Include the built artifacts for GitHub Pages

---
+++ docs/precache-manifest.319360dada1f15226080dcbbc4fb8bf3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b332de830a8ed7e7fe525a4aeb60104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ab992ead655463253ef",
+    "url": "/static/css/2.193a939a.chunk.css"
+  },
+  {
+    "revision": "b6ef6111b81cff14cb41",
+    "url": "/static/css/main.8f0528cc.chunk.css"
+  },
+  {
+    "revision": "1ab992ead655463253ef",
+    "url": "/static/js/2.32083436.chunk.js"
+  },
+  {
+    "revision": "b6ef6111b81cff14cb41",
+    "url": "/static/js/main.563a6ed7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
